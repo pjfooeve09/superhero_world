0620cdde91acc644401801e01c94a96f8031673b
Parham Javadi <parham@Parhams-MacBook-Pro.local>
2022-08-10 19:10:20 -0700

    added styling

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 
-
+gem 'bootstrap-sass', '~>3.3', '>= 3.3.7'
 
 gem 'pry'
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,13 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+
+//= require jquery
+//= require bootstrap-sprockets
+//= require_tree
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
